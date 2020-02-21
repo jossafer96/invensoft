ae0681b7745098884b0f8ea9ff230e27e656d5f4
jossafer96 <jossafer96@gmail.com>
2020-02-21 15:55:03 -0600

    Update asing-view.php

--- core/app/view/asing-view.php
+++ core/app/view/asing-view.php
@@ -57,37 +57,7 @@ label {
 }
 
 </style>
-<script>
-  $( function() {
-    var availableTags = [
-      "ActionScript",
-      "AppleScript",
-      "Asp",
-      "BASIC",
-      "C",
-      "C++",
-      "Clojure",
-      "COBOL",
-      "ColdFusion",
-      "Erlang",
-      "Fortran",
-      "Groovy",
-      "Haskell",
-      "Java",
-      "JavaScript",
-      "Lisp",
-      "Perl",
-      "PHP",
-      "Python",
-      "Ruby",
-      "Scala",
-      "Scheme"
-    ];
-    $( "#tags" ).autocomplete({
-      source: availableTags
-    });
-  } );
-  </script>
+
 
 <section class="content">
 
